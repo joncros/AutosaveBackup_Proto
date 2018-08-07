6955981b8af1a797716eb7bb34e1905fdaf5f488
Jonathan Croskell <jon.cros@gmail.com>
2018-08-06 21:45:58 -0600

    Initial commit

---
+++ src/com/joncros/github/autosavebackup_proto/Backup.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joncros.github.autosavebackup_proto;
+
+import java.nio.file.Path;
+
+/**
+ * 
+ * @author Jonathan Croskell
+ */
+class Backup {
+    /**
+     * Write a copy of a file to the directory it is located in
+     * @param path java.nio.file.Path Location of the file
+     */
+    void write(Path path) {
+        //todo
+    }
+    
+    private static String generateBackupFileName(Path path, String originalFileName) {
+        //todo
+        return "";
+    }
+}
