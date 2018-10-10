db209602a8e60074f1a9a6ac84af83bff0bd450a
Jonathan Croskell <jon.cros@gmail.com>
2018-10-10 17:20:38 -0600

    Folder and filefilter are now instance variables. Separated thread logic into start() method. Logic to ask for folder and filename if not provided as cl args.

--- src/com/joncros/github/autosavebackup_proto/AutosaveBackup.java
+++ src/com/joncros/github/autosavebackup_proto/AutosaveBackup.java
@@ -23,12 +23,30 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 /**
- * Simple command-line application to monitor a folder for game Autosaves and 
- * create backups of them.
+ * Simple command-line application for Windows to monitor a folder for a single 
+ * Autosave or other file with a specific filename, creating a backup each time
+ * the file is modified.
  * @author Jonathan Croskell
  */
 public class AutosaveBackup {
     private static final Logger logger = LogManager.getLogger();
+    private final Path folder;
+    private final IOFileFilter filter;
+    private static final String USAGE = 
+            "\nusage: AutosaveBackup [path to folder] [filename] will start the "
+            + "program with the specified folder and filename\n";
+    
+    public AutosaveBackup (Path folder, String filename) {
+        if (!Files.isDirectory(folder)) {
+            throw new IllegalArgumentException("Path does not point to an "
+                    + "existing directory");
+        }
+        this.folder = folder;
+        if (filename.isEmpty()) {
+            throw new IllegalArgumentException("filename string is empty");
+        }
+        this.filter = new NameFileFilter(filename, IOCase.INSENSITIVE);
+    }
     
     /**
      * @param args the command line arguments
@@ -36,23 +54,58 @@ public class AutosaveBackup {
      * Second argument is the file name to watch for.
      */
     public static void main(String[] args) {
-        String usage = "\nusage: AutosaveBackup [path to folder] [filename]\n";
-      
         logger.trace("arguments: {}", Arrays.toString(args));
-        try {
-            if (args.length < 1 
-                    || args.length > 2
-                    || !Files.isDirectory(Paths.get(args[0]))) {
-                System.out.print(usage);
-                return;
-            }    
+        
+        Path folder = Paths.get("");
+        String filename = "";
+        boolean validInput = false;
+        
+        /* 
+        * If two or more command line args provided and first arg is a valid 
+        * path, obtain filename from remaining args.
+        */
+        if (args.length >= 2
+                && (folder = validateFolderPath(args[0])) != null) {
+            validInput = true;
+            filename = args[1];
+            
+            // for case where spaces exist in the filename, so args.length > 2
+            for (int i = 2; i < args.length; i++) {
+                filename = filename + " " + args[i];
+            }
         }
-        catch (InvalidPathException e) {
-            System.out.print("\nPath to folder is invalid");
-            System.out.print(usage);
-            return;
+        else {
+            System.out.print(USAGE);
+        }
+        while (!validInput) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Type the full path to the folder to monitor:");
+            folder = validateFolderPath(in.nextLine());
+            if (folder == null) {
+                //didn't get a valid folder path, prompt for folder again
+                continue;
+            }
+            System.out.println("Type the filename (including extension) of "
+                + "the file to watch for");
+            filename = in.nextLine();
+            validInput = true;
         }
         
+        //remove any ' or " charagers if user surrounded filename in quotes
+        char first = filename.charAt(0);
+        char last = filename.charAt(filename.length() - 1);
+        if (first == '\'' || first == '\"'
+                && 
+                last == '\'' || last == '\"') {
+            filename = filename.substring(1, filename.length()-1);
+        }
+        
+        AutosaveBackup ab = new AutosaveBackup(folder, filename);
+        ab.start();
+    }
+    
+    public void start() {
+        logger.traceEntry();
         ExecutorService es = Executors.newFixedThreadPool(2);
         
         /* 
@@ -60,8 +113,6 @@ public class AutosaveBackup {
         * ConsoleInputTask thread has ended, prompting main to continue.
         */
         CountDownLatch countDownLatch = new CountDownLatch(1);
-        Path folder = Paths.get(args[0]);
-        IOFileFilter filter = new NameFileFilter(args[1], IOCase.INSENSITIVE);
         try {
             //Start Watcher
             Future<?> watcherFuture = 
@@ -92,5 +143,29 @@ public class AutosaveBackup {
             logger.trace("main() finally reached");
             es.shutdownNow();
         }
-    }    
+        logger.traceExit();
+    }
+    
+    /*
+    * Helper that 
+    * @returns a Path corresponding to the String, or null if the Path does not
+    * point to an existing folder or an InvalidPathException is encountered
+    */
+    private static Path validateFolderPath(String pathString) {
+        try {
+            Path path = Paths.get(pathString);
+            if (Files.isDirectory(path)) {
+                return path;
+            }
+            else {
+                System.out.println("Error: location is not a directory or does"
+                        + " not exist");
+                return null;
+            }
+        }
+        catch (InvalidPathException e) {
+            System.out.println("Error: illegal characters in path.");
+            return null;
+        }
+    }
 }
